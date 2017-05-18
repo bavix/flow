d31cab77bb0c8a2798b5c087efb2a7cc4c274c09
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-05-18 15:39:49 +0300

    simple

--- src/Flow/Flow.php
+++ src/Flow/Flow.php
@@ -2,7 +2,44 @@
 
 namespace Bavix\Flow;
 
+use Bavix\FlowNative\FlowNative;
+
 class Flow
 {
+
+    /**
+     * @var string
+     */
+    protected $ext = '.flow';
+
+    /**
+     * @var Lexer
+     */
+    protected $lexer;
+
+    /**
+     * @var FlowNative
+     */
+    protected $native;
+
+    /**
+     * Flow constructor.
+     *
+     * @param FlowNative $native
+     */
+    public function __construct(FlowNative $native)
+    {
+        $this->native = $native;
+        $this->lexer = new Lexer();
+    }
+
+    /**
+     * @param string $view
+     */
+    public function compile($view)
+    {
+        $path = $this->native->path($view . '.' . $this->ext);
+        // todo
+    }
 
 }
